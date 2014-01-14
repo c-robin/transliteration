9f0e5bb4965599b545a680651a4c6038cb951863
Alexandre Bérard <eskaev@gmail.com>
2014-01-14 15:52:42 +0100

    Report: continued SMT

--- moses/moses.sh
+++ moses/moses.sh
@@ -26,11 +26,11 @@ cd lm/
 $IRSTLM/bin/add-start-end.sh < ../output/translit_$LABEL.out > \
     translit_$LABEL.sb.out
 $IRSTLM/bin/build-lm.sh -i translit_$LABEL.sb.out -t ./tmp \
-    -p -s improved-kneser-ney -o translit_$LABEL.lm.out
+    -p -s improved-kneser-ney -o translit_$LABEL.lm.out -n 3
 $IRSTLM/bin/compile-lm --text translit_$LABEL.lm.out.gz \
     translit_$LABEL.arpa.out
 $MOSES_DIR/bin/build_binary translit_$LABEL.arpa.out \
-    translit_$LABEL.blm.out
+    translit_$LABEL.blm.out -i
 
 # Word alignment
 cd $CUR_DIR
@@ -38,7 +38,7 @@ mkdir -p working
 cd working/
 $MOSES_DIR/scripts/training/train-model.perl -root-dir \
     train_$LABEL -corpus ../output/translit_$LABEL -f input -e \
-    out -alignment grow-diag-final-and -reordering msd-bidirectional-fe -lm \
+    out -alignment grow-diag-final -reordering monotone --max-phrase-length 3 -lm \
     0:3:$CUR_DIR/lm/translit_$LABEL.blm\.out:8 -external-bin-dir \
     $GIZA_DIR >& training_$LABEL.out 
 fi
@@ -55,5 +55,4 @@ MODEL=working/train_$LABEL/model/moses.ini
 #$MOSES_DIR/scripts/training/filter-model-given-input.pl $TEST_INPUT $MODEL $TEST_TRUTH
 
 $MOSES_DIR/bin/moses -f $MODEL -distortion-limit 0 < $TEST_INPUT > $TEST_OUTPUT
-$MOSES_DIR/scripts/generic/multi-bleu.perl -lc $TEST_TRUTH < $TEST_OUTPUT
 fi
